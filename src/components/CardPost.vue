0458361ee691657a08a5ea9de7838517da1af85e
Théo Gillet <gillettheo1@gmail.com>
2022-06-09 11:50:15 +0200

    finish like

---
+++ src/components/CardPost.vue
@@ -0,0 +1,86 @@
+<script>
+export default {
+    name: 'CardPost',
+    props: {
+        post: {
+            type: Object,
+            required: true
+        }
+    },
+    methods: {
+        handleLike() {
+            this.$emit('like')
+        }
+    }
+}
+</script>
+<template>
+    <div class="cardPost">
+        <div class="cardPost-img">
+            <img :src="post.img" alt="">
+        </div>
+        <h3 class="cardPost-title">{{ post.title }}</h3>
+        <p class="cardPost-description">{{ post.description }}</p>
+        <div class="cardPost-like">
+            <button @click="handleLike">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-balloon-heart-fill" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M8.49 10.92C19.412 3.382 11.28-2.387 8 .986 4.719-2.387-3.413 3.382 7.51 10.92l-.234.468a.25.25 0 1 0 .448.224l.04-.08c.009.17.024.315.051.45.068.344.208.622.448 1.102l.013.028c.212.422.182.85.05 1.246-.135.402-.366.751-.534 1.003a.25.25 0 0 0 .416.278l.004-.007c.166-.248.431-.646.588-1.115.16-.479.212-1.051-.076-1.629-.258-.515-.365-.732-.419-1.004a2.376 2.376 0 0 1-.037-.289l.008.017a.25.25 0 1 0 .448-.224l-.235-.468ZM6.726 1.269c-1.167-.61-2.8-.142-3.454 1.135-.237.463-.36 1.08-.202 1.85.055.27.467.197.527-.071.285-1.256 1.177-2.462 2.989-2.528.234-.008.348-.278.14-.386Z"/>
+            </svg>
+            Like
+            </button>
+            <p>{{ post.like }} {{ post.like <= 1 ? 'Like' : 'Likes'}} </p>
+        </div>
+    </div>
+</template>
+
+<style lang="scss">
+.cardPost {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-around;
+    width: 500px;
+    height: 700px;
+    margin: 50px;
+    padding: 20px;
+    backdrop-filter: blur(10px);
+    background-color: rgba(255, 255, 255, 1);
+    border-radius: 26px;
+    box-shadow: 35px 35px 68px 0px rgba(87, 87, 87, 0.5), inset -0px -0px 16px 0px rgba(87, 87, 87, 0.6), inset 0px 11px 28px 0px rgb(255, 255, 255);
+
+    &-img{
+        width: 100%;
+        height: 50%;
+        border-radius: 26px;
+        
+        img{
+            width: 100%;
+            height: 100%;
+            border-radius: 26px;
+        }
+    }
+
+    &-like{
+        display: flex;
+        align-items: center;
+        justify-content: space-around;
+        width: 50%;
+
+        button{
+            width: 100px;
+            height: 30px;
+            border: none;
+            background-color: #F56040;
+            color: #fff;
+            font-size: 1.2rem;
+            border-radius: 10px;
+            transition: background-color 0.1s ease-in;
+
+            &:hover{
+                cursor: pointer;
+                background-color: #FD1D1D;
+            }
+        }
+    }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,77 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <div class="title-insta">
+      <h1>GramInsta</h1>
+
+    </div>
+    <div class="container-post">
+      <CardPost :post="post" @like="post.like ++" v-for="post in posts" :key='post.title'/>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import CardPost from "@/components/CardPost.vue";
 
 export default {
   name: 'HomeView',
   components: {
-  }
+    CardPost,
+  },
+  data(){
+    return {
+      posts: [
+        {
+          img: 'https://picsum.photos/200',
+          title: 'Post 1',
+          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+          like: 0,
+        },
+        {
+          img: 'https://picsum.photos/200',
+          title: 'Post 2',
+          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+          like: 0,
+        },
+        {
+          img: 'https://picsum.photos/200',
+          title: 'Post 3',
+          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+          like: 0,
+        },
+        {
+          img: 'https://picsum.photos/200',
+          title: 'Post 4',
+          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+          like: 0,
+        },
+      ]
+    }
+}
 }
 </script>
+
+<style lang="scss">
+.title-insta{
+  background: rgb(64,93,230);
+  background: linear-gradient(180deg, rgba(64,93,230,1) 0%, rgba(88,81,219,1) 11%, rgba(131,58,180,1) 22%, rgba(193,53,132,1) 33%, rgba(225,48,108,1) 44%, rgba(253,29,29,1) 55%, rgba(245,96,64,1) 66%, rgba(247,119,55,1) 77%, rgba(252,175,69,1) 88%, rgba(255,220,128,1) 99%);
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  h1{
+    font-size: 100px;
+    margin: 0;
+  }
+}
+.container-post {
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  flex-wrap: wrap;
+  width: 100%;
+  height: 100%;
+}
+</style>
